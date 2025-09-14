50632dbe195af4baed8658ec02220e575258f5cf
Lucas Araújo <73756840+ayohbit@users.noreply.github.com>
2025-09-14 09:45:49 -0300

    arquitetura inicial e modelo de dados

--- app/src/main/java/com/androidexpress/netflixremake/MainActivity.kt
+++ app/src/main/java/com/androidexpress/netflixremake/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.androidexpress.netflixremake
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.androidexpress.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
+
+    // m-v-c (model - [view/controller] activity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.i("Teste", "onCreate")
+
+        val movies = mutableListOf<Movie>()
+        for(i in 0 until 5) {
+            val movie = Movie("https://exemplo.com/$i.jpg")
+            movies.add(movie)
+        }
+
+        val adapter = MainAdapter(movies)
+        val rv: RecyclerView = findViewById(R.id.rv_main)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
     }
+
 }
---
+++ app/src/main/java/com/androidexpress/netflixremake/model/Movie.kt
@@ -0,0 +1,3 @@
+package com.androidexpress.netflixremake.model
+
+data class Movie(val coverUrl: String)
