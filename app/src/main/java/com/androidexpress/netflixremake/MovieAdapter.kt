abe39b68bfb34808d495d153e958894484dca35b
Lucas Araújo <73756840+ayohbit@users.noreply.github.com>
2025-09-15 09:49:40 -0300

    atualizando UI e criando tela de filmes

--- app/src/main/java/com/androidexpress/netflixremake/MovieAdapter.kt
+++ app/src/main/java/com/androidexpress/netflixremake/MovieAdapter.kt
@@ -4,14 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import com.androidexpress.netflixremake.model.Movie
 
 // Aqui é lista HORIZONTAL
-class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+class MovieAdapter(
+    private val movies: List<Movie>,
+    @LayoutRes private val layoutId: Int
+) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
         return MovieViewHolder(view)
     }
 
@@ -29,7 +33,7 @@ class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<Movie
             val imageCover: ImageView = itemView.findViewById(R.id.img_cover)
 
             // TODO: aqui vai ser trocado por uma URL que virá do servidor
-            // imageCover.setImageResource(movie.coverUrl)
+//             imageCover.setImageResource(movie.coverUrl)
         }
     }
 
